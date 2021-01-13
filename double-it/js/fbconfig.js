621afe82816e4ccc2b467cffc1f1019a488c0045
Sébastien Matos <sess22@gmail.com>
2021-01-13 09:18:45 +0100

    version 13 janvier seb

--- double-it/js/fbconfig.js
+++ double-it/js/fbconfig.js
@@ -41,8 +41,41 @@ DATABASE.ref("player-1/position").on("value", (snapshot) => {
 
 DATABASE.ref('messages/sendBall').on('value', (snapshot) => {
     const data = snapshot.val();
-    if (data.id != id)
-        createBall(-50, 200, 3);
+    if (initialized && data.id != id) {
+        const b = new Ball(-50, 200, data.speed, data.radius);
+        b.acceleration = createVector(data.accX, data.accY);
+        BALLS.push(b)
+    }
+    /*     createBall(-50, 200, 3); */
+})
+DATABASE.ref('messages/won').on('value', (snapshot) => {
+    const data = snapshot.val();
+    if (initialized && data.id != id) {
+        document.getElementById("win").style.opacity = 1;
+    }
+})
+
+DATABASE.ref('score1').on('value', (snapshot) => {
+    const data = snapshot.val();
+    console.log(data)
+    try {
+        document.getElementsByClassName("score")[0].innerText = data.score1;
+    } catch {
+        window.addEventListener("load", () => {
+            document.getElementsByClassName("score")[0].innerText = data.score1;
+        })
+    }
+})
+DATABASE.ref('score2').on('value', (snapshot) => {
+    const data = snapshot.val();
+    console.log(data)
+    try {
+        document.getElementsByClassName("score")[1].innerText = data.score2;
+    } catch {
+        window.addEventListener("load", () => {
+            document.getElementsByClassName("score")[1].innerText = data.score2;
+        })
+    }
 })
 
 // WRITE
--- double-it/js/main.js
+++ double-it/js/main.js
@@ -1,116 +1,161 @@
 let popSound;
-
+let firstBallEmitted = false;
 let jumper;
+let score = 0;
+SEND_MESSAGE("score1", { score1: score });
+SEND_MESSAGE("score2", { score2: score });
 
 let BALLS = [];
 
 let id = Math.floor(Math.random() * 10000000);
+let time = Date.now();
 
 let spring1 = new Spring();
 let handleLeft = {
-    x: new Spring(),
-    y: 100,
+  x: new Spring(),
+  y: 100,
 };
 let handleRight = {
-    x: new Spring(),
-    y: 100,
+  x: new Spring(),
+  y: 100,
 };
 
 let upSpring = new Spring();
 
-
-
 let groundLevel = 0.5;
 
 function setup() {
-    soundFormats('mp3');
-    popSound = loadSound('assets/pop-1.mp3');
-    pixelDensity(1);
-    createCanvas(windowWidth, windowHeight);
+  soundFormats("mp3");
+  // popSound = loadSound('assets/pop-1.mp3');
+  pixelDensity(1);
+  createCanvas(windowWidth, windowHeight);
 
-    jumper = new Jumper();
-    BALLS.push(new Ball(-50, 100, 3));
+  jumper = new Jumper();
 
-    noStroke();
+  /* BALLS.push(new Ball(-50, 100, 3)); */
+
+  noStroke();
+}
+document.addEventListener("keydown", (e) => {
+  if (e.key == "Escape") {
+    setTimeout(function () {
+      window.location.reload();
+    });
+  }
+});
+
+function emit(forceEmit) {
+firstBallEmitted = true;
+  let emitRate = forceEmit ? true : Math.random() < 0.006;
+
+  if (player == 1 && emitRate) {
+    createBall(-50, 100, 3);
+  }
 }
 
 function draw() {
+  if (document.getElementById("win").style.opacity > 0) {
+    document.getElementById("win").style.opacity -= 0.01;
+  }
+  if (document.getElementById("loss").style.opacity > 0) {
+    document.getElementById("loss").style.opacity -= 0.01;
+  }
 
+  time = Date.now();
+
+  background(220);
+  if (initialized) {
     push();
-    background(220);
     fill(255);
     drawGround();
+
+    // if (!firstBallEmitted) emit(true);
 
     translate(jumper.pos.x, jumper.pos.y);
     drawPlayer();
     pop();
+  }
 
+  jumper.pos.x = mouseX;
 
-    jumper.pos.x = mouseX;
+  //   if (player == 1 && Math.random() < 0.006) {
+  //     createBall(-50, 100, 3);
+  //   }
+  emit()
 
-    if (mouseIsPressed) {
-        jumper.jump(15);
+  if (mouseIsPressed) {
+    jumper.jump(15);
+  }
+
+  jumper.run();
+
+  for (let ball of BALLS) {
+    ball.run();
+    if (ball.destroyFlag) {
+      ball.destroy();
     }
-
-    jumper.run();
-
-    for (let ball of BALLS) {
-        ball.run();
-    }
+  }
 }
 
 function fillGradient() {
-    let ctx = drawingContext;
-    // Create gradient
-    let grd = ctx.createLinearGradient(0, 100, 0, -100);
-    grd.addColorStop(0, "black");
-    grd.addColorStop(1, "white");
-    // Fill with gradient
-    ctx.fillStyle = grd;
+  let ctx = drawingContext;
+  // Create gradient
+  let grd = ctx.createLinearGradient(0, 100, 0, -100);
+  grd.addColorStop(0, "black");
+  grd.addColorStop(1, "white");
+  // Fill with gradient
+  ctx.fillStyle = grd;
 }
 
 function drawPlayer() {
-    translate(0, -50);
+  translate(0, -50);
 
-    spring1.update(mouseX);
-    handleLeft.x.update(mouseX);
-    handleRight.x.update(mouseX);
-    upSpring.update(jumper.pos.y);
+  spring1.update(mouseX);
+  handleLeft.x.update(mouseX);
+  handleRight.x.update(mouseX);
+  upSpring.update(jumper.pos.y);
 
-    //stroke("blue");
-    fillGradient();
-    beginShape();
+  //stroke("blue");
+  fillGradient();
+  beginShape();
 
-    vertex(-100, 100);
-    bezierVertex(-100,
-        100, -handleLeft.x.pos - 100 + mouseX,
-        handleLeft.y + upSpring.transform(jumper.pos.y),
-        spring1.pos - mouseX, -100 + upSpring.transform(jumper.pos.y)
-    );
-    vertex(spring1.pos - mouseX, -100);
-    bezierVertex(
-        spring1.pos - mouseX, -100 + upSpring.transform(jumper.pos.y), -handleRight.x.pos + 100 + mouseX,
-        handleRight.y + upSpring.transform(jumper.pos.y),
-        100,
-        100
-    );
-    vertex(100, 100);
-    endShape(CLOSE);
+  vertex(-100, 100);
+  bezierVertex(
+    -100,
+    100,
+    -handleLeft.x.pos - 100 + mouseX,
+    handleLeft.y + upSpring.transform(jumper.pos.y),
+    spring1.pos - mouseX,
+    -100 + upSpring.transform(jumper.pos.y)
+  );
+  vertex(spring1.pos - mouseX, -100);
+  bezierVertex(
+    spring1.pos - mouseX,
+    -100 + upSpring.transform(jumper.pos.y),
+    -handleRight.x.pos + 100 + mouseX,
+    handleRight.y + upSpring.transform(jumper.pos.y),
+    100,
+    100
+  );
+  vertex(100, 100);
+  endShape(CLOSE);
 }
 
 function mousePressed() {
-    // jumper.jump();
-    // BALLS.push(new Ball())
+  // jumper.jump();
+  // BALLS.push(new Ball())
 }
 
-function createBall(x, y, speed) {
-    BALLS.push(new Ball(x, y, speed))
+function createBall(x, y, speed, radius = 50) {
+  let b = new Ball(x, y, speed, radius);
+  BALLS.push(b);
+  return b;
 }
 
 function drawGround() {
-    rect(0, groundLevel * height, width, height);
+  rect(0, groundLevel * height, width, height);
 }
 
 function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
+  resizeCanvas(windowWidth, windowHeight);
 }
